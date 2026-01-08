f92975a115804b46ced8549a7ff6726b598fa923
Yahir Eduardo Bravo <yahir.eduardo.bravo@gmail.com>
2026-01-08 11:03:32 -0500

    feat: add hero component

---
+++ src/app/pages/hero/hero-page.component.ts
@@ -0,0 +1,27 @@
+import { UpperCasePipe } from '@angular/common';
+import { Component, computed, signal } from '@angular/core';
+
+@Component({
+  templateUrl: './hero-page.component.html',
+  imports: [UpperCasePipe],
+})
+export class HeroPageComponent {
+  name = signal('Tony Stark');
+  age = signal(45);
+
+  heroDescription = computed(() => `${this.name()} #${this.age()}`);
+
+  getHeroDescription() {
+    return `${this.name()} - ${this.age()}`;
+  }
+
+  changeHero() {
+    this.name.set('Spiderman');
+    this.age.set(23);
+  }
+
+  resetForm() {
+    this.name.set('Tony Stark');
+    this.age.set(45);
+  }
+}
