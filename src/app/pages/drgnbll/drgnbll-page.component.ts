9e9f6195950f98f8ac9233bf380c4d6b56d6dd06
Yahir Eduardo Bravo <yahir.eduardo.bravo@gmail.com>
2026-01-08 15:32:03 -0500

    feat(s5-c6): add conditional rendering

--- src/app/pages/drgnbll/drgnbll-page.component.ts
+++ src/app/pages/drgnbll/drgnbll-page.component.ts
@@ -1,4 +1,5 @@
-import { Component, signal } from '@angular/core';
+import { NgClass } from '@angular/common';
+import { Component, computed, signal } from '@angular/core';
 
 interface BCharacter {
   id: number;
@@ -8,11 +9,21 @@ interface BCharacter {
 
 @Component({
   templateUrl: './drgnbll-page.component.html',
+  imports: [
+    // NgClass
+  ]
 })
 export class DrgnbllPageComponent {
   characters = signal<BCharacter[]>([
     { id: 1, name: 'Goku', power: 9001 },
     { id: 2, name: 'Vegeta', power: 8000 },
     { id: 3, name: 'Piccolo', power: 3000 },
+    { id: 4, name: 'Yamcha', power: 900 },
   ]);
+
+  // powerClasses = computed(()=>{
+  //   return {
+  //     'pico-color-pink-500': true
+  //   }
+  // })
 }
