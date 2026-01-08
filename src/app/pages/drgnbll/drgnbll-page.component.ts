8d9f4e3bb46b6bd2492aae4d7cfe668bb48d3810
Yahir Eduardo Bravo <yahir.eduardo.bravo@gmail.com>
2026-01-08 15:57:46 -0500

    feat(s5-c6): add actions to add new character

--- src/app/pages/drgnbll/drgnbll-page.component.ts
+++ src/app/pages/drgnbll/drgnbll-page.component.ts
@@ -11,9 +11,12 @@ interface BCharacter {
   templateUrl: './drgnbll-page.component.html',
   imports: [
     // NgClass
-  ]
+  ],
 })
 export class DrgnbllPageComponent {
+  newCharacterName = signal('');
+  newCharacterPower = signal(0);
+
   characters = signal<BCharacter[]>([
     { id: 1, name: 'Goku', power: 9001 },
     { id: 2, name: 'Vegeta', power: 8000 },
@@ -26,4 +29,29 @@ export class DrgnbllPageComponent {
   //     'pico-color-pink-500': true
   //   }
   // })
+  addCharacter() {
+    if (
+      //
+      !this.newCharacterName() ||
+      !this.newCharacterPower() ||
+      this.newCharacterPower() <= 0
+    ) {
+      return;
+    }
+
+    const newCharacter: BCharacter = {
+      id: this.characters().length + 1,
+      name: this.newCharacterName(),
+      power: this.newCharacterPower(),
+    };
+
+    // this.characters().push(newCharacter);
+    this.characters.update((lst) => [...lst, newCharacter]);
+    this.reseteFields()
+  }
+
+  reseteFields(){
+    this.newCharacterPower.set(0)
+    this.newCharacterName.set("")
+  }
 }
