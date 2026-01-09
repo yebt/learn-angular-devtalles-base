fc6d1939ae933fe943b55289cfdfb41d653b637a
Yahir Eduardo Bravo <yahir.eduardo.bravo@gmail.com>
2026-01-09 11:34:31 -0500

    feat(s5): change the routering strategy

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,10 +2,17 @@ import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
+    provideRouter(routes),
+
+    // Hash strategy
+    {
+      provide: LocationStrategy,
+      useClass: HashLocationStrategy
+    }
   ]
 };
